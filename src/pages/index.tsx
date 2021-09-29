5a15171fbdf7b9dec6373d769d83ed3c781f0395
Jason Karjadi <jason@karjadi.com>
2021-09-30 00:49:59 +0900

    cleaner-index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Box, Center, HStack, Icon, IconButton } from "@chakra-ui/react";
-import { FC, MouseEvent, useEffect, useRef } from "react";
+import { FC, MouseEvent, useEffect, useRef, useState } from "react";
 import {
   AdditiveBlending,
   BackSide,
@@ -24,8 +24,17 @@ import {
 } from "../shaders";
 
 const Index: FC = () => {
-  const itemARef = useRef(""),
-    itemBRef = useRef("");
+  const [isLocaleMode, setIsLocaleMode] = useState(true),
+    [selected, setSelected] = useState(["", ""]);
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(tick);
+    addEventListener("resize", () => setResize());
+    return () => {
+      cancelAnimationFrame(requestRef.current);
+      removeEventListener("resize", () => setResize);
+    };
+  }, []);
 
   const requestRef = useRef(0),
     rendRef = useRef<WebGLRenderer>(),
@@ -47,7 +56,66 @@ const Index: FC = () => {
     groupRef = useRef(new Group()),
     sceneRef = useRef(new Scene()),
     mouseRef = useRef(new Vector2()),
-    rayRef = useRef(new Raycaster());
+    rayRef = useRef(new Raycaster()),
+    onCanvasLoaded = useRef((canvas: HTMLCanvasElement & HTMLDivElement) => {
+      rendRef.current = new WebGLRenderer({ canvas });
+      ctrlRef.current = new OrbitControls(camRef.current, canvas);
+      globeRef.current = new Mesh(
+        new SphereGeometry(radRef.current, 50, 50),
+        new ShaderMaterial({
+          vertexShader,
+          fragmentShader,
+          uniforms: {
+            globeTexture: {
+              value: new TextureLoader().load("/globe.jpg"),
+            },
+          },
+        })
+      );
+
+      rendRef.current.setPixelRatio(devicePixelRatio);
+      camRef.current.position.setZ(18);
+      Object.assign(ctrlRef.current, {
+        enableDamping: true,
+        rotateSpeed: 0.5,
+        autoRotate: true,
+        autoRotateSpeed: 0.2,
+        enablePan: false,
+        enableZoom: false,
+      });
+      globeRef.current.rotateY(-Math.PI / 2);
+      sceneRef.current.add(
+        groupRef.current.add(globeRef.current),
+        atmRef.current
+      );
+      setResize();
+
+      createPoint(
+        "INA",
+        0.9140165430070886,
+        -0.013775448070460912,
+        -0.4054429628687974
+      );
+      // Indonesia -0.7893, 113.9213
+      // 0.9140165430070886, -0.013775448070460912, -0.4054429628687974
+      createPoint(
+        "JPN",
+        0.5372766576225415,
+        0.5906732692963204,
+        -0.6020289711573246
+      );
+      // Japan 36.2048, 138.2529
+      // 0.5372766576225415, 0.5906732692963204, -0.6020289711573246
+      createPoint(
+        "ENG",
+        -0.034051759463686306,
+        0.822919263139182,
+        0.5671369887706965
+      );
+      // United Kingdom 55.3781, -3.436
+      // -0.034051759463686306, 0.822919263139182, 0.5671369887706965
+    });
+  const ptRef = useRef<Mesh<SphereGeometry, MeshBasicMaterial>[]>([]);
 
   const setResize = () => {
       camRef.current.aspect = innerWidth / innerHeight;
@@ -57,128 +125,44 @@ const Index: FC = () => {
     setMouseVector = (e: MouseEvent) => {
       mouseRef.current.x = (e.clientX / innerWidth) * 2 - 1;
       mouseRef.current.y = -(e.clientY / innerHeight) * 2 + 1;
-    };
-  const setPointRaycaster = (
-    points: Mesh<SphereGeometry, MeshBasicMaterial>[]
-  ) => {
-    rayRef.current.setFromCamera(mouseRef.current, camRef.current);
-    const intersects = rayRef.current.intersectObjects(points);
-    if (intersects.length > 0) {
-      if (!itemARef.current) {
-        itemARef.current = intersects[0].object.name;
-        // console.log("logic x", itemARef.current, itemBRef.current);
-      } else if (intersects[0].object.name !== itemARef.current) {
-        if (!itemBRef.current) {
-          itemBRef.current = intersects[0].object.name;
-          // console.log("logic y", itemARef.current, itemBRef.current);
-        } else if (intersects[0].object.name !== itemBRef.current) {
-          itemBRef.current = intersects[0].object.name;
-          // console.log("logic z", itemARef.current, itemBRef.current);
-        }
+    },
+    tick = () => {
+      rayRef.current.setFromCamera(mouseRef.current, camRef.current);
+      if (rayRef.current.intersectObject(globeRef.current!).length > 0) {
+        ctrlRef.current!.autoRotate = false;
+      } else {
+        ctrlRef.current!.autoRotate = true;
       }
-      // console.log("intersects with", intersects[0].object.name);
-    }
-  };
-  const tick = () => {
-    rayRef.current.setFromCamera(mouseRef.current, camRef.current);
-    if (rayRef.current.intersectObject(globeRef.current!).length > 0) {
-      ctrlRef.current!.autoRotate = false;
-    } else {
-      ctrlRef.current!.autoRotate = true;
-    }
-    ctrlRef.current!.update();
-    rendRef.current!.render(sceneRef.current, camRef.current);
-    requestAnimationFrame(tick);
-  };
-
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(tick);
-    addEventListener("resize", () => setResize());
-    return () => {
-      cancelAnimationFrame(requestRef.current);
-      removeEventListener("resize", () => setResize);
-      removeEventListener("mousedown", () => setPointRaycaster);
+      ctrlRef.current!.update();
+      rendRef.current!.render(sceneRef.current, camRef.current);
+      requestAnimationFrame(tick);
     };
-  }, []);
-
-  const onCanvasLoaded = (canvas: HTMLCanvasElement & HTMLDivElement) => {
-    if (!canvas) {
-      return;
-    }
-
-    rendRef.current = new WebGLRenderer({ canvas });
-    ctrlRef.current = new OrbitControls(camRef.current, canvas);
-    rendRef.current.setPixelRatio(devicePixelRatio);
-    camRef.current.position.setZ(18);
-    Object.assign(ctrlRef.current, {
-      enableDamping: true,
-      rotateSpeed: 0.5,
-      autoRotate: true,
-      autoRotateSpeed: 0.2,
-      enablePan: false,
-      enableZoom: false,
-    });
-    setResize();
-
-    globeRef.current = new Mesh(
-      new SphereGeometry(radRef.current, 50, 50),
-      new ShaderMaterial({
-        vertexShader,
-        fragmentShader,
-        uniforms: {
-          globeTexture: {
-            value: new TextureLoader().load("/globe.jpg"),
-          },
-        },
-      })
-    );
-    globeRef.current.rotation.y = -Math.PI / 2;
-    sceneRef.current.add(
-      groupRef.current.add(globeRef.current),
-      atmRef.current
-    );
-
-    const points: Mesh<SphereGeometry, MeshBasicMaterial>[] = [];
-    const createPoint = (name: string, x: number, y: number, z: number) => {
+  const createPoint = (name: string, x: number, y: number, z: number) => {
       const point = new Mesh(
         new SphereGeometry(0.2, 50, 50),
-        new MeshBasicMaterial({
-          color: "#ffffff",
-        })
+        new MeshBasicMaterial()
       );
       const radCoord = (coord: number) => radRef.current * coord;
       point.position.set(radCoord(x), radCoord(y), radCoord(z));
       point.name = name;
       groupRef.current.add(point);
-      points.push(point);
+      ptRef.current.push(point);
+    },
+    setPointRaycaster = () => {
+      rayRef.current.setFromCamera(mouseRef.current, camRef.current);
+      const intersects = rayRef.current.intersectObjects(ptRef.current);
+      if (intersects.length > 0) {
+        if (!selected[0]) {
+          setSelected([intersects[0].object.name, selected[1]]);
+        } else if (intersects[0].object.name !== selected[0]) {
+          if (!selected[1]) {
+            setSelected([selected[0], intersects[0].object.name]);
+          } else if (intersects[0].object.name !== selected[1]) {
+            setSelected([selected[0], intersects[0].object.name]);
+          }
+        }
+      }
     };
-    createPoint(
-      "INA",
-      0.9140165430070886,
-      -0.013775448070460912,
-      -0.4054429628687974
-    );
-    // Indonesia -0.7893, 113.9213
-    // 0.9140165430070886, -0.013775448070460912, -0.4054429628687974
-    createPoint(
-      "JPN",
-      0.5372766576225415,
-      0.5906732692963204,
-      -0.6020289711573246
-    );
-    // Japan 36.2048, 138.2529
-    // 0.5372766576225415, 0.5906732692963204, -0.6020289711573246
-    createPoint(
-      "ENG",
-      -0.034051759463686306,
-      0.822919263139182,
-      0.5671369887706965
-    );
-    // United Kingdom 55.3781, -3.436
-    // -0.034051759463686306, 0.822919263139182, 0.5671369887706965
-
-    addEventListener("mousedown", () => setPointRaycaster(points));
-  };
 
   const TopBar: FC = () => {
       return (
@@ -263,7 +247,12 @@ const Index: FC = () => {
   return (
     <>
       <TopBar />
-      <Box as="canvas" ref={onCanvasLoaded} onMouseMove={setMouseVector} />
+      <Box
+        as="canvas"
+        ref={onCanvasLoaded.current}
+        onMouseMove={setMouseVector}
+        onMouseDown={setPointRaycaster}
+      />
       <BottomBar />
     </>
   );
