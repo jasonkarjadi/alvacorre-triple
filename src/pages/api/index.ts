3ae01780f3861609457ee8de748302994d670b90
Jason Karjadi <jason@karjadi.com>
2021-10-02 17:54:05 +0900

    remove-api-routes

--- src/pages/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { locales } from "../../data";
-
-const handler = (_: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json(locales);
-};
-
-export default handler;
