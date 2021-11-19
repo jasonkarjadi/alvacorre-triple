1bf91b2ed81c3e7dd3e68775cc50bf445e4d1c12
Jason Karjadi <jason@karjadi.com>
2021-11-19 13:56:24 +0900

    convert-syntax

--- i18n.js
+++ i18n.js
@@ -1,4 +1,4 @@
-module.exports = {
+const i18n = {
   defaultLocale: "en",
   locales: ["en", "jp", "id"],
   pages: {
@@ -9,3 +9,5 @@ module.exports = {
     "/creator": ["creator", "creatorFill"],
   },
 };
+
+export default i18n;
