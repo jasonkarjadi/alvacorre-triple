3b19911849371af599c83094a6ee8b822e90e32d
Jason Karjadi <jason@karjadi.com>
2022-01-03 04:51:57 +0900

    fix-pic

--- src/pages/creator.tsx
+++ src/pages/creator.tsx
@@ -3,7 +3,7 @@ import useTranslation from "next-translate/useTranslation";
 import Head from "next/head";
 import Image from "next/image";
 import { FC } from "react";
-import ProfilePicture from "../../public/DSC_8106.jpg";
+import ProfilePicture from "../../public/JasonKarjadi.jpg";
 import { ContentWrap } from "../components/ContentWrap";
 
 const CreatorPage: FC = () => {
@@ -21,15 +21,19 @@ const CreatorPage: FC = () => {
         {t("navbar:creator").toUpperCase()}
       </Heading>
       <Flex>
-        <Box mr={3}>
+        <Box>
           <Box w={32} h={32} overflow="hidden" border="solid tan">
-            <Image src={ProfilePicture} alt="Picture of Author" />
+            <Image
+              src={ProfilePicture}
+              alt="Picture of Author"
+              placeholder="blur"
+            />
           </Box>
           <Text textAlign="center">{"Jason Karjadi"}</Text>
+          <Text textAlign="center">jason@karjadi.com</Text>
         </Box>
         <Box>
           <Text w="full">{t("body")}</Text>
-          <Text>jason@karjadi.com</Text>
         </Box>
       </Flex>
     </ContentWrap>
